eff02721d567178773bd41ed75cb542736f0b018
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2020-10-02 15:29:32 -0300

    montagem da agenda

---
+++ system/agenda/includes/settings.php
@@ -0,0 +1,16 @@
+<?php
+/* updated via install/index.php on Wed, 30 Sep 2020 15:56:57 -0400
+install_password: e10adc3949ba59abbe56e057f20f883e
+db_type: mysqli
+db_host: 127.0.0.0.1
+db_database: agneda
+db_login: root
+db_persistent: false
+readonly: false
+user_inc: user.php
+use_http_auth: false
+single_user: false
+db_password: 123456
+db_cachedir: /tmp
+# end settings.php */
+?>
